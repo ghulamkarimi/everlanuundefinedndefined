cb61e6dc8cfe1723557f4a4c44015f7f2e3ece64
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-01-09 17:08:14 +0100

    fertig section 7

---
+++ src/components/section7/SectionSeven.tsx
@@ -0,0 +1,20 @@
+const SectionSeven = () => {
+  return (
+    <div className="flex flex-wrap gap-4 justify-center items-center">
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <p>Cleaner fashion</p>
+        <img src="/section7/image-sec7-2.png" alt="" />
+        <p>See the sustainability efforts behind each of our products.</p>
+        <a className="underline">read more</a>
+      </div>
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <p>Our Holiday Gift Picks</p>
+        <img src="/section7/imageSec7-1.png" alt="" />
+        <p>The best presents for everyone on your list.</p>
+        <a className="underline">read more</a>
+      </div>
+    </div>
+  );
+};
+
+export default SectionSeven;
