219f365a13c0a1b7fbd5b51cccc0382f78caef3e
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-01-18 17:35:13 +0100

    sicher

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      padding: '42px',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      spacing: {
+        '18.5': '4.625rem',
+      }
+    },
   },
   plugins: [],
 }
