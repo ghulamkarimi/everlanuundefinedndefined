5679eea7b5fbf14c07054ec07310bb7eaf4f5be7
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-01-18 16:22:17 +0100

    nochmal

--- src/components/popular/Popular.tsx
+++ src/components/popular/Popular.tsx
@@ -27,12 +27,12 @@ const PopularImage: IImage[] = [
 ];
 
 const Popular = () => {
-    return (
-      <div className="flex flex-col justify-center items-center md:inline-flex md:text-start py-8 lg:px-[150px]  ">
-        <h4 className="text-center md:text-start w-full">Popular Categories</h4> 
-        
+  return (
+    <div className="flex flex-col justify-center items-center md:inline-flex md:text-start py-8 lg:px-[150px]  ">
+      <h4 className="text-center md:text-start w-full">Popular Categories</h4>
+
       <div>
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 ">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 ">
           {PopularImage.map((item) => (
             <div key={item.title} className="">
               <img src={item.image} alt={item.title} />
@@ -41,8 +41,8 @@ const Popular = () => {
           ))}
         </div>
       </div>
-      </div>
-    );
-  };
-  
-  export default Popular;
+    </div>
+  );
+};
+
+export default Popular;
